3c6adbed0c55bc49b5f86c0bc87695d4791d821d
quocdunginfo <quocdunginfo@gmail.com>
2014-05-06 15:22:12 +0700

    base
    Chon duoc diem de tinh khoang cach

---
+++ khoangCach/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace khoangCach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_totalDistance = new System.Windows.Forms.Label();
+            this.richTextBox_log = new System.Windows.Forms.RichTextBox();
+            this.button_clearLog = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(828, 498);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::khoangCach.Properties.Resources.Ansnet;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(3000, 2400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.button_clearLog);
+            this.panel2.Controls.Add(this.richTextBox_log);
+            this.panel2.Controls.Add(this.label_totalDistance);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button_calculate);
+            this.panel2.Location = new System.Drawing.Point(828, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 498);
+            this.panel2.TabIndex = 1;
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_calculate.Location = new System.Drawing.Point(7, 12);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(148, 36);
+            this.button_calculate.TabIndex = 0;
+            this.button_calculate.Text = "Calculate";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            this.button_calculate.Click += new System.EventHandler(this.button_calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Distance:";
+            // 
+            // label_totalDistance
+            // 
+            this.label_totalDistance.AutoSize = true;
+            this.label_totalDistance.Location = new System.Drawing.Point(83, 103);
+            this.label_totalDistance.Name = "label_totalDistance";
+            this.label_totalDistance.Size = new System.Drawing.Size(45, 13);
+            this.label_totalDistance.TabIndex = 2;
+            this.label_totalDistance.Text = "[not set]";
+            // 
+            // richTextBox_log
+            // 
+            this.richTextBox_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_log.Location = new System.Drawing.Point(7, 130);
+            this.richTextBox_log.Name = "richTextBox_log";
+            this.richTextBox_log.Size = new System.Drawing.Size(148, 365);
+            this.richTextBox_log.TabIndex = 3;
+            this.richTextBox_log.Text = "";
+            // 
+            // button_clearLog
+            // 
+            this.button_clearLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_clearLog.Location = new System.Drawing.Point(7, 54);
+            this.button_clearLog.Name = "button_clearLog";
+            this.button_clearLog.Size = new System.Drawing.Size(148, 34);
+            this.button_clearLog.TabIndex = 4;
+            this.button_clearLog.Text = "Reset";
+            this.button_clearLog.UseVisualStyleBackColor = true;
+            this.button_clearLog.Click += new System.EventHandler(this.button_clearLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 496);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button_calculate;
+        private System.Windows.Forms.Label label_totalDistance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox_log;
+        private System.Windows.Forms.Button button_clearLog;
+
+    }
+}
+
---
+++ khoangCach/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace khoangCach
+{
+    public partial class Form1 : Form
+    {
+        class MousePoint
+        {
+            private int x;
+            private int y;
+            public MousePoint(int x, int y)
+            {
+                setX(x);
+                setY(y);
+            }
+            public int getX()
+            {
+                return x;
+            }
+            public int getY()
+            {
+                return y;
+            }
+            public void setX(int x)
+            {
+                this.x = x;
+            }
+            public void setY(int y)
+            {
+                this.y = y;
+            }
+            //
+            public double distanceFrom(MousePoint from)
+            {
+                double tmp = (double)(x - from.getX()) * (double)(x - from.getX()) + (double)(y - from.getY()) * (double)(y - from.getY());
+                return Math.Sqrt(tmp);
+            }
+            public MousePoint cloneNew()
+            {
+                MousePoint tmp = new MousePoint(this.getX(), this.getY());
+                return tmp;
+            }
+        }
+        private MousePoint current = null;
+        private ArrayList list = null;
+        public Form1()
+        {
+            InitializeComponent();
+            //init
+            current = new MousePoint(0, 0);
+            list = new ArrayList();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            current.setX(e.Location.X);
+            current.setY(e.Location.Y);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //add current point to list
+            list.Add(this.current.cloneNew());
+            richTextBox_log.Text += "Point "+list.Count+" ("+current.getX()+", "+current.getY()+")"+"\n";
+        }
+
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            //check
+            int total = 0;
+            MousePoint tmp_cur;
+            MousePoint tmp_prev;
+            if(list.Count>=2)
+            {
+                for(int i=1;i<list.Count;i++)
+                {
+                    tmp_prev = (MousePoint)list[i-1];
+                    tmp_cur = (MousePoint) list[i];
+                    total += (int)tmp_cur.distanceFrom(tmp_prev);
+                }
+            }
+            //display on view
+            label_totalDistance.Text = total.ToString();
+        }
+
+        private void button_clearLog_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            label_totalDistance.Text = "[not set]";
+            richTextBox_log.Clear();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+        private MousePoint drag_begin = new MousePoint(0,0);
+        private MousePoint drag_end = new MousePoint(0, 0);
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag_begin.setX(e.Location.X);
+            drag_begin.setY(e.Location.Y);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag_end.setX(e.Location.X);
+            drag_end.setY(e.Location.Y);
+            //calculate drag
+            /*
+            int XMove = drag_end.getX()-drag_begin.getX();
+            int YMove = drag_end.getY() - drag_begin.getY();
+            int maxH = panel1.HorizontalScroll.Maximum;
+            int minH = panel1.HorizontalScroll.Minimum;
+            int maxV = panel1.VerticalScroll.Maximum;
+            int minV = panel1.VerticalScroll.Minimum;
+            //to Right
+            if(XMove>0)
+            {
+                if(YMove>0)
+                {
+                    //to Down
+                    if(panel1.HorizontalScroll.Value-XMove>=minH)
+                    {
+                        panel1.HorizontalScroll.Value -= XMove;
+                    }
+                    if(panel1.VerticalScroll.Value-YMove>=minV)
+                    {
+                        panel1.VerticalScroll.Value -= YMove;
+                    }
+                }
+                else
+                {
+                    YMove = -YMove;
+                    //to Up
+                    if (panel1.HorizontalScroll.Value - XMove >= minH)
+                    {
+                        panel1.HorizontalScroll.Value -= XMove;
+                    }
+                    if (panel1.VerticalScroll.Value + YMove <= minV)
+                    {
+                        panel1.VerticalScroll.Value += YMove;
+                    }
+                }
+            }
+            //to Left
+            else
+            {
+                XMove = -XMove;
+                if (YMove > 0)
+                {
+                    //to Down
+                    if (panel1.HorizontalScroll.Value + XMove <= minH)
+                    {
+                        panel1.HorizontalScroll.Value += XMove;
+                    }
+                    if (panel1.VerticalScroll.Value - YMove >= minV)
+                    {
+                        panel1.VerticalScroll.Value -= YMove;
+                    }
+                }
+                else
+                {
+                    YMove = -YMove;
+                    //to Up
+                    if (panel1.HorizontalScroll.Value + XMove <= minH)
+                    {
+                        panel1.HorizontalScroll.Value += XMove;
+                    }
+                    if (panel1.VerticalScroll.Value + YMove <= minV)
+                    {
+                        panel1.VerticalScroll.Value += YMove;
+                    }
+                }
+            }
+             * */
+
+        }
+    }
+}
